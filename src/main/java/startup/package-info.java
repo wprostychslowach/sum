8d5864bf19d39751753350667aaae11ad7ecb0c3
wprostychslowach <me@wprostychslowach.pl>
2020-04-06 16:56:15 +0200

    Extend package info description

--- src/main/java/startup/package-info.java
+++ src/main/java/startup/package-info.java
@@ -1,2 +1,2 @@
-/* Main & Composition root for app so the module depends on all other modules. It could be done with container as well */
+/* Main & Composition root for app so the module depends on all other modules. It could be done with container as well with Register-Resolve-Release pattern */
 package startup;
